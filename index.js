d0bafe95ce34ebcd9ef89fc2cf81b77c3dc46f15
Abdullahi Mohammed <odanwu211@gmail.com>
2023-04-05 16:32:35 +0100

    to-do list first commit

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
-const todoArray = [];
-const containerEl = document.getElementById("container");
-const headerEl = document.querySelector("header");
-const todoContentEl = document.querySelector(".todo_content");
-const inputContainerEl = document.querySelector(".input_container");
-const inputEl = document.querySelector(".input_box");
-const inputBtnEl = document.querySelector(".input_btn");
-const listContainerEl = document.querySelector(".todo_list_container");
-const unorderedListEl = document.querySelector(".unordered_list");
-const errorMsgEl = document.querySelector(".error_msg");
-        const todoListEl = document.createElement("li");
-const trashCanEl = document.createElement("p");
+const inputEl = document.querySelector(".input_box"),
+  inputBtnEl = document.querySelector(".input_btn"),
+  errorMsgEl = document.querySelector(".error_msg"),
+  unorderedListEl = document.querySelector(".unordered_list");
+
+const storedValue = localStorage.getItem("todoValues");
+let todoItems = [];
+
+if (storedValue) {
+  todoItems = JSON.parse(storedValue);
+
+  todoItems.forEach((item) => {
+    createTodoItem(item);
+  });
+}
+
 inputBtnEl.addEventListener("click", () => {
-	if(inputEl.value === "") {
-		errorMsgEl.innerHTML = "Please enter your To-Do!";
-		setTimeout( () => {
-errorMsgEl.style.display="none";
-		}, 3000);
-	}else {
-const todoTextEl = document.createElement("p");
-	trashCanEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';trashCanEl.classList.add("trash_can_style");
-	todoTextEl.textContent = inputEl.value;
-	todoListEl.append(todoTextEl);
-	unorderedListEl.append(todoListEl);
-	inputEl.value = "";
-	todoListEl.append(trashCanEl);
-	}
-})
+  const todoValue = inputEl.value;
+  const inputFormat = /[A-Z a-z]$/;
 
-trashCanEl.addEventListener("click", () => {
-
+  if (todoValue.trim() === "") {
+    errorMsgEl.innerHTML = "Please input your to-do!";
+    errorMsgEl.classList.add("error_msg");
+    setTimeout(() => {
+      errorMsgEl.style.display = "none";
+    }, 3000);
+    errorMsgEl.style.display = "block";
+  } else {
+    createTodoItem(todoValue);
+    inputEl.value = "";
+    todoItems.push(todoValue);
+    localStorage.setItem("todoValues", JSON.stringify(todoItems));
+  }
 });
+
+function createTodoItem(item) {
+  const todoListItem = document.createElement("li");
+  todoListItem.classList.add("list_style");
+
+  const todoTextEl = document.createElement("span");
+  todoTextEl.innerText = item;
+  todoListItem.appendChild(todoTextEl);
+  todoListItem.addEventListener("click", () => {
+    todoListItem.classList.add("complete_task");
+  });
+  const trashCanEl = document.createElement("button");
+  trashCanEl.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+  trashCanEl.classList.add("trash_can_style");
+  todoListItem.appendChild(trashCanEl);
+
+  unorderedListEl.appendChild(todoListItem);
+
+  trashCanEl.addEventListener("dblclick", () => {
+    todoItems = todoItems.filter((value) => value !== item);
+    localStorage.setItem("todoValues", JSON.stringify(todoItems));
+    todoListItem.remove();
+  });
+}
